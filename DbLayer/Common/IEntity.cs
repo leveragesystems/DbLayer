699d9f52b95bd77234572b015d7a726e4fb3b282
Carel <dutoit.carel7@gmail.com>
2013-02-11 23:42:41 +0200

    Mongo db added using repository patern generic
    Signed-off-by: Carel <dutoit.carel7@gmail.com>

---
+++ DbLayer/Common/IEntity.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace DbLayer.Common
+{
+    public interface IEntity
+    {
+        int Id { get; set; }
+        Guid Uid { get; set; }
+        DateTime DateCreated { get; set; }
+        string CreatedBy { get; set; }
+
+        bool IsNew();
+        //void AddModificationHistory(ModificationHistory modificationHistory);
+    }
+}
---
+++ DbLayer/Repositories/IRepository.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Linq;
+using System.Linq.Expressions;
+
+namespace DbLayer.Repositories {
+  public interface IRepository<T,U> : IQueryable<T> where T:Entity<U> {
+    bool Create<T>(T entity);
+    bool Delete<T>(T entity);
+    T Update<T>(string id, T entity);
+    IQueryable<T> SearchFor(Expression<Func<T, bool>> predicate);
+    IQueryable<T> Get();
+    T Get(int id);
+  }
+}
