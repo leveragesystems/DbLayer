a79d48febf00f7c4850d03382ad27175b51179f9
Devlin Schoonraad <devlin.schoonraad@gmail.com>
2013-03-23 13:46:03 +0200

    Refactoring

--- DbLayer/Common/Entity.cs
+++ DbLayer/Common/Entity.cs
@@ -1,6 +1,8 @@
-﻿
-using DbLayer.Models;
+﻿using DbLayer.Models;
+using MongoDB.Bson.Serialization.Attributes;
+using MongoDB.Bson.Serialization.IdGenerators;
+
 public abstract class Entity<U> {
-	[Validate(RegexClass.Required, "Please provide the Id")]
+  [Validate(RegexClass.Required, "Please provide the Id")]
   public U Id { get; set; }
 }
